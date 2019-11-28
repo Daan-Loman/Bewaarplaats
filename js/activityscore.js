a18858ae4b05c1d858950474d9f21ae4db7675e9
Daan-Loman <56959707+Daan-Loman@users.noreply.github.com>
2019-11-28 14:35:04 +0100

    Add files via upload

---
+++ js/activityscore.js
@@ -0,0 +1,62 @@
+        // Teken de grafiek
+        var ctx = document.getElementById('mybarChart').getContext('2d');
+        var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'horizontalBar',
+        // The data for our dataset
+        // ['jan', 'feb', 'maa', 'apr', 'mei', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec'],
+        data: {
+            labels: ["Likes", "Reacties", "Shares"],
+                    datasets: [{
+                        label: 'Aantal logins',
+                        data: [40,30,20],
+                        backgroundColor: "rgba(168, 54, 155, 0.05)",
+                        borderColor: 'rgba(168, 54, 155,1)',
+                        borderWidth: 1
+                        }]
+                    },
+                options:{
+                    aspectRatio: 3.3,
+                    legend: {
+                        display: false,
+                    },
+                    tooltips: {
+                        backgroundColor: "rgb(255,255,255)",
+                        bodyFontColor: "#858796",
+                        titleMarginBottom: 10,
+                        titleFontColor: '#6e707e',
+                        titleFontSize: 14,
+                        borderColor: '#dddfeb',
+                        borderWidth: 1,
+                        xPadding: 15,
+                        yPadding: 15,
+                        displayColors: false,
+                        intersect: false,
+                        mode: 'index',
+                        caretPadding: 10
+                    },
+                    scales: {
+                      xAxes: [{
+                        gridLines: {
+                          display: false,
+                          drawBorder: false
+                        }
+                      }],
+                      yAxes: [{
+                        gridLines: {
+                          color: "rgb(234, 236, 244)",
+                          zeroLineColor: "rgb(234, 236, 244)",
+                          drawBorder: false,
+                          autoSkip: true,
+                          borderDash: [2],
+                          zeroLineBorderDash: [2]
+                        },
+                        ticks: {
+                          beginAtZero: true,
+                          autoSkip: true,
+                          maxTicksLimit: 5
+                        }
+                      }],
+                    }
+                }
+    })
